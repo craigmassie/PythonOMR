0ac547b13843ea9e1feb2935660df18566ae2221
Craig Massie <2207400m@student.gla.ac.uk>
2019-02-25 11:40:39 +0000

    Increased readability of the code

--- check_finder.py
+++ check_finder.py
@@ -4,43 +4,45 @@ import sys
 from matplotlib import pyplot as plt
 
 def plt_img(img):
-    plt.figure(figsize = (30,30))
+    plt.figure(figsize = (40,40))
     plt.imshow(img,'gray')
     plt.show()
     
 def im_threshold(img):
+    # Thresholds light greys to white, and inverses the page to black.
     _, thresh = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY_INV)
+
     # img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
     #         cv2.THRESH_BINARY,11,2)
+
     return thresh
 
 def check_find(img, threshhold, mark_thresh, check_type):
     contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    font = cv2.FONT_HERSHEY_COMPLEX
+    font = cv2.FONT_HERSHEY_TRIPLEX
     document_height, document_width = img.shape[0], img.shape[1]
     mark_thresh = float(mark_thresh.strip('%')) / 100.0
     
     for cnt in contours:
         approx = cv2.approxPolyDP(cnt, 0.1*cv2.arcLength(cnt, True), True)
         coords = approx.ravel()
-        x = coords[0]
-        y = coords[1]
 
         #If quadrilateral
         if len(approx) == 4:
-            feature_height, feature_width = (coords[5] - coords[1]), (coords[4] - coords[0])
+            x, y, x2, y2 = coords[0], coords[1], coords[4], coords[5]
+            feature_height, feature_width = (y2 - y), (x2 - x)
             #If the size of the quadrilateral found is significant (e.g. not hidden inside text)
             if feature_width > float(document_width)/100 and feature_height > float(document_width)/100:
                 #If a square (± 5 pixels)
                 if abs(feature_height - feature_width) < 5:
-                    crop_img = img[coords[1]:coords[1] + feature_height, coords[0]:coords[0] + feature_width]
+                    crop_img = img[y : y + feature_height, x : x + feature_width]
+                    #Thresholds the image to binary black and white
                     _, crop_thresh = cv2.threshold(crop_img, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
                     total = crop_img.shape[0] * crop_img.shape[1]
                     count_black = total - cv2.countNonZero(crop_thresh)
                     if count_black > float(total)*mark_thresh and (check_type == "filled" or check_type == "all"):
-                        # plt_img(crop_img)
                         cv2.drawContours(img, [approx], 0, (0), 2)
-                        cv2.putText(img, "Filled", (x, y), font, 0.5, (0))
+                        cv2.putText(img, "Filled", (x, y), font, 1, (0))
                     elif check_type == "empty" or check_type == "all":
                         cv2.drawContours(img, [approx], 0, (0), 2)
                         cv2.putText(img, "Empty", (x, y), font, 0.5, (0))
@@ -55,3 +57,4 @@ if __name__ == "__main__":
     threshold = im_threshold(img)
     img = check_find(img, threshold, mark_thresh, check_type)
     plt_img(img)
+    cv2.imwrite('edited.png', img)
